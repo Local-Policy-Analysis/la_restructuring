79d8f6c499617b8d0c38cbc6ec4bf9c255c3aef5
JamesCaddick <57677871+JamesCaddick@users.noreply.github.com>
2023-03-15 15:09:16 +0000

    changed to merge LA names rather than replace

--- restructure.py
+++ restructure.py
@@ -10,16 +10,19 @@ def restructure(df, from_year, to_year):
     Must be smaller than to_year.
     to_year: integer representing the financial year of the desired LA structure. Must be larger than from_year.
     
-    returns: dataframe post restructuring
+    returns: dataframe post restructuring with ecode, ons_code overwritten and class and authority columns merged
+    
+    requisites: import pandas as pd
     '''
-    assert to_year > from_year, 'to year is greater than from year'
+    assert to_year > from_year, 'to_year is less than from_year'
     url = 'https://github.com/JamesCaddick/la_strucuture_lookup/blob/main/la_structure.xlsx?raw=true'
     df_lookup = pd.read_excel(url)
     dict_ons = pd.Series(df_lookup[f'ons_code_{to_year}'].values, index=df_lookup[f'ons_code_{from_year}']).to_dict()
     dict_ecode = pd.Series(df_lookup[f'ecode_{to_year}'].values, index=df_lookup[f'ecode_{from_year}']).to_dict()
-    dict_authority = pd.Series(df_lookup[f'authority_{to_year}'].values, index=df_lookup[f'authority_{from_year}']).to_dict()
-    dict_class = pd.Series(df_lookup[f'class_{to_year}'].values, index=df_lookup[f'class_{from_year}']).to_dict()
-    df = df.replace(dict_ons).replace(dict_ecode).replace(dict_authority).replace(dict_class)
+    df = (df
+          .replace(dict_ons).replace(dict_ecode)
+          .merge(df_lookup[[f'class_{to_year}', f'ons_code_{to_year}', f'authority_{to_year}']], left_on='ons_code', right_on=f'ons_code_{to_year}', how='left')
+          .drop(columns=f'ons_code_{to_year}'))
     return df
 
 df = pd.read_excel('to_restructure.xlsx')
