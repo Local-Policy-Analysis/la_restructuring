355ea2d6fbb66981ed7c095a20e65df247a7acd9
JamesCaddick <57677871+JamesCaddick@users.noreply.github.com>
2023-03-16 10:32:10 +0000

    remove duplicates when merging

--- restructure.py
+++ restructure.py
@@ -3,16 +3,17 @@ import pandas as pd
 def restructure(df, from_year, to_year):
     '''
     Function to change the local authority structure from a given start point to a new year's structure. Will replace old
-    authority names, la class, ons code and ecode to new structure
+    ons code and ecode to new codes, and appends columns with new LA name and LA class
     
     df: dataframe to be restructured
     from_year: integer representing the financial year of the starting LA structure. Note financial year 2022-23 would be 2022. 
     Must be smaller than to_year.
     to_year: integer representing the financial year of the desired LA structure. Must be larger than from_year.
     
-    returns: dataframe post restructuring with ecode, ons_code overwritten and class and authority columns merged
+    returns: dataframe post restructuring
     
     requisites: import pandas as pd
+    # to do: LA names were taken from ONS code history database but there are duplicate names since for e.g. Essex refers to the SC and the FRA
     '''
     assert to_year > from_year, 'to_year is less than from_year'
     url = 'https://github.com/JamesCaddick/la_strucuture_lookup/blob/main/la_structure.xlsx?raw=true'
@@ -21,8 +22,12 @@ def restructure(df, from_year, to_year):
     dict_ecode = pd.Series(df_lookup[f'ecode_{to_year}'].values, index=df_lookup[f'ecode_{from_year}']).to_dict()
     df = (df
           .replace(dict_ons).replace(dict_ecode)
-          .merge(df_lookup[[f'class_{to_year}', f'ons_code_{to_year}', f'authority_{to_year}']], left_on='ons_code', right_on=f'ons_code_{to_year}', how='left')
-          .drop(columns=f'ons_code_{to_year}'))
+          .merge(df_lookup[[f'class_{to_year}', f'ons_code_{to_year}', f'authority_{to_year}']].drop_duplicates(),
+                 left_on='ons_code',
+                 right_on=f'ons_code_{to_year}',
+                 how='left')
+          .drop(columns=f'ons_code_{to_year}')
+          )
     return df
 
 df = pd.read_excel('to_restructure.xlsx')
